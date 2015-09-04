c3e021e2773b56abb1a772059b982ccc56fb898c
Mike Lucek <michael.s.lucek@gmail.com>
2015-09-04 11:46:16 -0400

    logo and mailer

--- app.rb
+++ app.rb
@@ -1,5 +1,41 @@
 require 'sinatra'
 require 'mail'
+require "mailer.rb"
+
+def mailer
+	puts "MAILER SAYS"
+	puts @email
+	puts @text
+
+	options = { :address              => "mail.classwork.mike-lucek.com",
+	            :port                 => 587,
+	            :user_name            => 'test@classwork.mike-lucek.com',
+	            :password             => 'tung99tung99',
+	            :authentication       => 'plain',
+	            :enable_starttls_auto => false  }
+
+
+	Mail.defaults do
+	  delivery_method :smtp, options
+	end
+
+	email = @email
+	text = @text
+
+	mail = Mail.new do
+	  from    'test@classwork.mikelucek.com'
+	  to      email
+	  subject 'This is a test email'
+	  body    text
+	end
+
+	mail.to_s
+
+	mail.deliver
+end
+
+
+
 
 
 get "/" do 
